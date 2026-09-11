3ac9c9f2d83b563fabb60c477ce671fac19e2ca9
pict88 <computerpartsae@gmail.com>
2026-09-12 02:32:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-echo "Changing to root"
+echo "[>] Changing *.sh and *.env to root"
 chown root:root *.sh *.env
 
-echo "Updating privileges"
+echo "[>] Updating privileges"
 chmod 744 *.sh
 
-echo "Moving script.sh to /etc/NetworkManager/dispatcher.d/"
+echo "[>] Moving script.sh to /etc/NetworkManager/dispatcher.d/"
 mv script.sh /etc/NetworkManager/dispatcher.d/
 
-echo "Moving enable-lab.sh disable-lab.sh recovery.sh to /usr/local/bin/"
+echo "[>] Moving enable-lab.sh disable-lab.sh recovery.sh to /usr/local/bin/"
 mv enable-lab.sh disable-lab.sh recovery.sh usb-auth.sh /usr/local/bin/
 
-echo "Moving lab-config.env to /etc/"
+echo "[>] Moving lab-config.env to /etc/"
 mv lab-config.env /etc/
-
-echo "installation done successfully"
+echo ""
+echo "[+] Installation done successfully run sudo enable-lab.sh"
