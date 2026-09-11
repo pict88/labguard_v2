6f74bb6638dac180c08ae072e53aaea590a40554
pict88 <computerpartsae@gmail.com>
2026-09-12 02:11:51 +0530

    Update script.sh
    better log using $(date) function

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ ERROR_LOG="/home/pict/Desktop/Error.log"
 if [ -f /etc/lab-config.env ]; then
     source /etc/lab-config.env
 else
-    echo "[-] CRITICAL: Configuration file /etc/lab-config.env missing!" >> $ERROR_LOG
+    echo "[$(date)] CRITICAL: Configuration file /etc/lab-config.env missing!" >> $ERROR_LOG
     exit 1
 fi
 
@@ -26,14 +26,14 @@ fi
 
 if [ ! -d "$FLAG" ]; then
 	echo "----------------------------------------------------------------------------------" >> $ERROR_LOG
-    	echo "$(date) and [$UID] - Dispatcher fired: $IFACE $EVENT" >> $ERROR_LOG       
+    	echo "[$(date)] UID [$UID] - Dispatcher fired: $IFACE $EVENT" >> $ERROR_LOG       
     	echo -e "mode: NORMAL \n----------------------------------------------------------------------------------" >> $ERROR_LOG
     exit 0
 fi
 
 ip addr add $HELPER_IP dev $IFACE
    
-curl -s --connect-timeout 5 --max-time 10 -d "ip=$HELPER_IP&module=network&action=alert&status=global_attempted" http://$SERVER_SOCKET/server.php
+curl -s -d "ip=$HELPER_IP&module=network&action=alert&status=global_attempted" http://$SERVER_SOCKET/server.php
 
 export DISPLAY=:0
 USER_INPUT=$(timeout 10 sudo -u $user env XDG_RUNTIME_DIR=/run/user/$(id -u $user) \
@@ -50,15 +50,16 @@ response=$(curl -s -d "ip=$HELPER_IP&module=network&action=auth&password=$USER_I
 
 if [ "$response" = "200" ]; then
 	curl -s -d "ip=$HELPER_IP&module=network&action=alert&status=authenticated" http://$SERVER_SOCKET/server.php
-	echo "User authenticated with server response $response. Cleaning up exam artifacts..." >> $ERROR_LOG
+	echo "[$(date)] ATTENTION: User authenticated with server response $response and password $USER_INPUT. Cleaning up exam artifacts..." >> $ERROR_LOG
 	rm -rf /home/$user/Desktop/FLAG &> /dev/null
 	echo 'ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ENV{ID_USB_INTERFACES}=="*:08*:*", ATTR{authorized}="1"' | sudo tee /etc/udev/rules.d/99-usb-disable-lab.rules 2>/dev/null
 	rm /etc/udev/rules.d/99-usb-enable-lab.rules
 	sudo udevadm control --reload-rules && sudo udevadm trigger 
 else
-	echo "INTERFACE DOWN with password $USER_INPUT" >> $ERROR_LOG	
+	echo "[$(date)] INTERFACE DOWN with password $USER_INPUT" >> $ERROR_LOG	
 	curl -s -d "ip=$HELPER_IP&module=network&action=alert&status=isolated" http://$SERVER_SOCKET/server.php
 	nmcli device disconnect $IFACE
 fi
 
 ip addr del $HELPER_IP dev $IFACE
+
